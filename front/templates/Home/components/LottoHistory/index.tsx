df4db503f4931af0025e76ae96d6ed056a197d5f
Vegetable_builder <riona1209@outlook.com>
2024-01-07 17:18:06 -0300

    feat: loading and optimisic update after claim

--- front/templates/Home/components/LottoHistory/index.tsx
+++ front/templates/Home/components/LottoHistory/index.tsx
@@ -1,13 +1,6 @@
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { RoundInfoCard } from "@/components/RoundInfoCard";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
+
 import {
   Dialog,
   DialogContent,
@@ -16,15 +9,12 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { useAccount } from "@/contexts/AccountContext";
 import { useContract } from "@/contexts/ContractContext";
-import { toNumber } from "ethers";
-import { Coins, TrendingUp, Trophy } from "lucide-react";
-import moment from "moment";
+
+import { TrendingUp } from "lucide-react";
 
 export const LottoHistory = () => {
-  const { lastRounds, claim } = useContract();
-  const { address } = useAccount();
+  const { lastRounds } = useContract();
 
   return (
     <Dialog>
@@ -43,58 +33,7 @@ export const LottoHistory = () => {
 
         <div className="flex gap-4 flex-col">
           {lastRounds.map((round: any) => (
-            <Card key={round.id}>
-              <CardHeader>
-                <CardTitle>LottoTicket</CardTitle>
-                <CardDescription>
-                  Started Time:{" "}
-                  {moment(toNumber(round.startedTime) * 1000).format(
-                    "MM/DD/YYYY HH:mm:ss"
-                  )}
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="flex flex-col gap-2">
-                  <div className="flex items-center justify-between">
-                    <span className="text-neutral-400">Winner</span>
-                    <span className="text-neutral-400">{round.winner}</span>
-                  </div>
-                  <div className="flex items-center justify-between">
-                    <span className="text-neutral-400">Status</span>
-                    <span className="text-neutral-400">
-                      {round.claimed ? "Claimed" : "Not claimed"}
-                    </span>
-                  </div>
-                  <div className="flex items-center justify-between">
-                    <span className="text-neutral-400">Date</span>
-                    <span className="text-neutral-400">
-                      {moment(toNumber(round.timeToFinish) * 1000).format(
-                        "MM/DD/YYYY HH:mm:ss"
-                      )}
-                    </span>
-                  </div>
-                </div>
-              </CardContent>
-              <CardFooter>
-                {round.winner.toLowerCase() == address.toLowerCase() &&
-                  !round.claimed && (
-                    <div className=" flex items-center space-x-4 rounded-md border p-4 w-full">
-                      <Trophy />
-                      <div className="flex-1 space-y-1">
-                        <p className="text-sm font-medium leading-none">
-                          You won!
-                        </p>
-                        <p className="text-sm text-muted-foreground">
-                          Claim your prize!
-                        </p>
-                      </div>
-                      <Button variant="default" onClick={() => claim(round.id)}>
-                        Claim
-                      </Button>
-                    </div>
-                  )}
-              </CardFooter>
-            </Card>
+            <RoundInfoCard key={round.key} round={round} />
           ))}
         </div>
       </DialogContent>
